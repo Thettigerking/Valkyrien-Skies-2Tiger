cc2773a41fbe304d801761bb3a88c718e99f7d4d
chaoticunnatural <j.thomps1310@gmail.com>
2026-06-18 11:53:31 -0400

    final cleanup

--- common/src/main/kotlin/org/valkyrienskies/mod/common/VSCrashReportHeader.kt
+++ common/src/main/kotlin/org/valkyrienskies/mod/common/VSCrashReportHeader.kt
@@ -28,13 +28,13 @@ object VSCrashReportHeader {
         "krunchitized",
         "successful pilgrimage to the shipyard",
         "perchanceably",
-        "'I did a quick test, it seemed alright'",
-        "lemme see, and then we carry the one. yup. and then add the remainder, and there we go. your ship is yeeted"
+        "'I did a quick test, it seemed alright.'",
+        "lemme see, and then we carry the one. yup. and then add the remainder, and there we go. your ship is yeeted."
     )
 
     private fun getHeaderComment(): String {
         return try {
-            HEADER_COMMENTS[(Util.getMillis() % HEADER_COMMENTS.size.toLong()).toInt()]
+            HEADER_COMMENTS[(Util.getMillis() % HEADER_COMMENTS.size).toInt()]
         } catch (_: Exception) {
             "...oh dear."
         }
@@ -45,7 +45,7 @@ object VSCrashReportHeader {
         builder.append("\n// " + getHeaderComment())
         builder.append("\nPlease check that this issue occurs without Valkyrien Skies before reporting it to other mod authors.")
         builder.append("\nIf this issue does not occur without Valkyrien Skies, please check if your issue has already been reported at https://github.com/ValkyrienSkies/Valkyrien-Skies-2/issues?q=is%3Aissue.")
-        builder.append("\nIf your issue has not been reported / you are unable to find it, or you are unsure, please report it at the link provided above.\n\n")
+        builder.append("\nIf your issue has not been reported or you are unsure, please report it at the link provided above.\n\n")
     }
 
 }
